abaefd3ce3ebe59fd6f84c735f4455960dd38fd2
Fernando de Leon <fdeleon@supresencia.com>
2024-03-05 12:04:43 -0500

    Creación del header básico

---
+++ src/components/header.jsx
@@ -0,0 +1,13 @@
+import icon_nxtwave from "../icons/logo_nxtwave.png";
+
+export const Header = () => {
+  return (
+    <>
+      <header>
+        <figure>
+          <img className="logo-nxtwave" src={icon_nxtwave} alt="icon-nxtwave" />
+        </figure>
+      </header>
+    </>
+  );
+};
