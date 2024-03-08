98941bf1548d49b5219bbce8ed08cb3b344c112c
Fernando de Leon <fdeleon@supresencia.com>
2024-03-08 09:32:02 -0500

    Se agrega video de fondo

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,11 +1,9 @@
-import slider from "../assets/gif/Te-siento-en-mi-ser.gif";
-
 const Slider = () => {
   return (
     <>
-      <figure>
-        <img className="slider1" src={slider} alt="slider" />
-      </figure>
+      <div className="slider1">
+        <p>we are the revolution</p>
+      </div>
     </>
   );
 };
