1e2d509a7c59643caee77fef47b0db8d9aedee56
Fernando de Leon <fdeleon@supresencia.com>
2024-03-08 12:40:32 -0500

    Se agrega el componente de historia

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -1,5 +1,6 @@
 import Header from "./Header";
 import Slider from "./Slider";
+import { History } from "./History";
 
 import { BrowserRouter } from "react-router-dom";
 import { CounterDown } from "./CounterDown";
@@ -7,12 +8,14 @@ import { CounterDown } from "./CounterDown";
 import "../styles/main.css";
 import "../styles/header.css";
 import "../styles/slider.css";
+import "../styles/history.css";
 
 const Main = () => {
   return (
     <BrowserRouter>
       <Header />
       <Slider />
+      <History />
       <CounterDown />
     </BrowserRouter>
   );
