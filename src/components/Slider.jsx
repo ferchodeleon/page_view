6d7055cd63cb54dadb1901a55b100f419da13e8e
Fernando de Leon <fdeleon@supresencia.com>
2024-03-08 08:14:16 -0500

    Parte inicial de slider

---
+++ src/components/Slider.jsx
@@ -0,0 +1,13 @@
+import slider from "../assets/gif/Te-siento-en-mi-ser.gif";
+
+const Slider = () => {
+  return (
+    <>
+      <figure>
+        <img className="slider1" src={slider} alt="slider" />
+      </figure>
+    </>
+  );
+};
+
+export default Slider;
--- src/components/main.jsx
+++ src/components/main.jsx
@@ -1,14 +1,18 @@
-import { BrowserRouter } from "react-router-dom";
 import Header from "./Header";
+import Slider from "./Slider";
+
+import { BrowserRouter } from "react-router-dom";
 import { CounterDown } from "./CounterDown";
 
 import "../styles/main.css";
 import "../styles/header.css";
+import "../styles/slider.css";
 
 const Main = () => {
   return (
     <BrowserRouter>
       <Header />
+      <Slider />
       <CounterDown />
     </BrowserRouter>
   );
