6f538735fb23797d0e7a8ae61344815ab4df5f98
Fernando de Leon <fdeleon@supresencia.com>
2024-03-12 15:55:19 -0500

    Se crea componente para sección de videos

---
+++ src/components/MusicList.jsx
@@ -0,0 +1,18 @@
+export const MusicList = () => {
+  return (
+    <>
+      <div className="container-video">
+        <iframe
+          width="660"
+          height="415"
+          src="https://www.youtube.com/embed/aW3u2ot3sss?si=N__Z9ObEcxNwbTvm"
+          title="YouTube video player"
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          allowfullscreen
+        />
+        <h2>we are the revolution</h2>
+      </div>
+    </>
+  );
+};
