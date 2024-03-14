a9aeaf37ea8354e76f2814f0bcf01d6932acf53f
Fernando de Leon <fdeleon@supresencia.com>
2024-03-14 12:28:27 -0500

    Se agrega botón de ver más para mostrar todos los parrafos

--- src/components/History.jsx
+++ src/components/History.jsx
@@ -1,39 +1,41 @@
+import { useState } from "react";
 import instragram from "../assets/icons/instagram.svg";
 import youtube from "../assets/icons/youtube.svg";
 import facebook from "../assets/icons/facebook.svg";
 import x from "../assets/icons/twitter.svg";
 
+const paragraphs = [
+  "Somos la generación que hace la diferencia, nos revelamos ante el molde mediocre de este mundo, somos contracultura, no nos conformamos con las modas o ideologías del momento.",
+  "Somos una generación revolucionaria, y con nuestra frente en alto reconocemos sin temor ni vergüenza que Dios es real y que está vivo. Avanzamos firmes porque sabemos que con nuestras pisadas temblará la tierra y que con nuestra voz su presencia desciende.",
+  "Conocemos la verdadera causa por la cual debemos luchar, y unidos somos un ejército devastador e incansable que está listo para la victoria. Aquella que conquistamos con ojos cerrados y con rodillas sobre la tierra. Nuestras voces traen una nueva canción; aquella que revela la perfecta paz.",
+  "Si morimos por Cristo, también viviremos con él. Si soportamos los sufrimientos, compartiremos su reinado. Si decimos que no lo conocemos, también él dirá que no nos conoce. Y aunque no seamos fieles, él permanece fiel; porque jamás rompe su promesa.",
+  "Tú y yo, juntos, ¡SOMOS NXTWAVE",
+  "Tú y yo, juntos, ¡SOMOS LA REVOLUCIÓN!",
+];
+
 export const History = () => {
+  const [showAll, setShowAll] = useState(false);
+
+  const toggleShowAll = () => {
+    setShowAll(!showAll);
+  };
+
+  const paragraphsToShow = showAll ? 1000 : 2;
+
   return (
     <div className="history-section">
       <div>
         <div className="container-history-text">
-          <p className="history-text">
-            Somos la generación que hace la diferencia, nos revelamos ante el
-            molde mediocre de este mundo, somos contracultura, no nos
-            conformamos con las modas o ideologías del momento.
-          </p>
-          <p className="history-text">
-            Somos una generación revolucionaria, y con nuestra frente en alto
-            reconocemos sin temor ni vergüenza que Dios es real y que está vivo.
-            Avanzamos firmes porque sabemos que con nuestras pisadas temblará la
-            tierra y que con nuestra voz su presencia desciende.
-          </p>
-          <p className="history-text">
-            Conocemos la verdadera causa por la cual debemos luchar, y unidos
-            somos un ejército devastador e incansable que está listo para la
-            victoria. Aquella que conquistamos con ojos cerrados y con rodillas
-            sobre la tierra. Nuestras voces traen una nueva canción; aquella que
-            revela la perfecta paz.
-          </p>
-          <p className="history-text">
-            Si morimos por Cristo, también viviremos con él. Si soportamos los
-            sufrimientos, compartiremos su reinado. Si decimos que no lo
-            conocemos, también él dirá que no nos conoce. Y aunque no seamos
-            fieles, él permanece fiel; porque jamás rompe su promesa.
-          </p>
-          <p className="history-text">Tú y yo, juntos, ¡SOMOS NXTWAVE</p>
-          <p className="history-text">Tú y yo, juntos, ¡SOMOS LA REVOLUCIÓN!</p>
+          <div>
+            {paragraphs.slice(0, paragraphsToShow).map((pragraph, index) => (
+              <p key={index} className="history-text">
+                {pragraph}
+              </p>
+            ))}
+          </div>
+          <button className="showParagraphs" onClick={toggleShowAll}>
+            {showAll ? "Ocultar" : "Ver más"}
+          </button>
         </div>
         <div className="history-socialmedia">
           <h2>¡Saber más!</h2>
