7cf1eb313caa97416282593a905d4201f9ff2259
Fernando de Leon <fdeleon@supresencia.com>
2024-03-14 14:42:31 -0500

    Cambio en tamaño de los iconos

--- src/components/MusicStreaming.jsx
+++ src/components/MusicStreaming.jsx
@@ -1,10 +1,10 @@
 import arrow from "../assets/gif/arrow.gif";
 import amazonMusic from "../assets/icons_music_platforms/amazon_music.svg";
-import appleMusic from "../assets/icons_music_platforms/apple_music.svg";
+import appleMusic from "../assets/icons_music_platforms/apple.png";
 import claroMusic from "../assets/icons_music_platforms/claro_music.svg";
 import deezer from "../assets/icons_music_platforms/deezer.svg";
 import spotify from "../assets/icons_music_platforms/spotify.svg";
-import youtubeMusic from "../assets/icons_music_platforms/youtube_music.svg";
+import youtube from "../assets/icons_music_platforms/youtube.png";
 
 export const MusicStreaming = () => {
   return (
@@ -18,8 +18,8 @@ export const MusicStreaming = () => {
         <div className="platforms">
           <img src={spotify} alt="icon spotify" />
           <img src={amazonMusic} alt="icon amazon music" />
-          <img src={youtubeMusic} alt="icon youtube music" />
-          <img src={appleMusic} alt="icon apple music" />
+          <img src={youtube} alt="icon youtube music" />
+          <img className="appleMusic" src={appleMusic} alt="icon apple music" />
           <img src={claroMusic} alt="icon claro music" />
           <img src={deezer} alt="icon deezer" />
         </div>
