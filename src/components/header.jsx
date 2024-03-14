ff4fc5ccc0a2d5260548ec85f5bab65f372f405c
Fernando de Leon <fdeleon@supresencia.com>
2024-03-14 08:59:52 -0500

    Ajustes del header en modo resposive mobile

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -3,10 +3,12 @@ import { useState, useEffect } from "react";
 
 import logo from "../assets/logo/logo-nxtwave-transparent.png";
 import icon_shop from "../assets/icons/shop.gif";
+import menu from "../assets/icons/hamburger.svg";
 
 const Header = () => {
   const [visible, setVisible] = useState(true);
   const [lastScrollTop, setLastScrollTop] = useState(0);
+  const [open, setOpen] = useState(true);
 
   const handleScroll = () => {
     const { scrollY } = window;
@@ -19,31 +21,45 @@ const Header = () => {
     setLastScrollTop(scrollY);
   };
 
+  const handleClick = () => {
+    setOpen(!open);
+  };
+
   useEffect(() => {
     window.addEventListener("scroll", handleScroll);
     return () => window.removeEventListener("scroll", handleScroll);
   });
 
   return (
-    <>
-      <header className={`active ${visible && "visible"}`}>
-        <figure>
-          <img src={logo} alt="logo_nxtwave" />
-        </figure>
-        <nav className="nav-menu">
-          <ol>
-            <li>Home</li>
-            <li>Fotos</li>
-            <li>Historia</li>
-            <li>Videos</li>
-            <li>Shop</li>
-            <Link className="button-icon-shop" to="">
-              <img className="icon-shop" src={icon_shop} alt="shop" />
-            </Link>
-          </ol>
-        </nav>
-      </header>
-    </>
+    console.log("OPEN", open),
+    (
+      <>
+        <header className={`active ${visible && "visible"}`}>
+          <figure>
+            <img src={logo} alt="logo_nxtwave" />
+          </figure>
+          <nav style={{ display: open && "none" }} className="nav-menu">
+            {/* <nav className={`${open ? "nav-menu-open" : "nav-menu"}`}> */}
+            <ol>
+              <li>Home</li>
+              <li>Fotos</li>
+              <li>Historia</li>
+              <li>Videos</li>
+              <li>Shop</li>
+              <Link
+                className="button-icon-shop"
+                to="https://developer.mozilla.org/es/docs/Web/CSS/box-shadow"
+              >
+                <img className="icon-shop" src={icon_shop} alt="shop" />
+              </Link>
+            </ol>
+          </nav>
+          <button onClick={handleClick} className="hamburger-menu">
+            <img src={menu} alt="menu-hamburgesa" />
+          </button>
+        </header>
+      </>
+    )
   );
 };
 
