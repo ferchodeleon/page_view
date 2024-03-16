1c9aec4fb927aab8117ad4eef9f833533b852e82
Fernando De Leon Gil <gasnare@hotmail.com>
2024-03-16 08:22:15 -0500

    El eliminan los iconos de redes sociales

--- src/components/History.jsx
+++ src/components/History.jsx
@@ -1,8 +1,4 @@
 import { useState } from "react";
-import instragram from "../assets/icons/instagram.svg";
-import youtube from "../assets/icons/youtube.svg";
-import facebook from "../assets/icons/facebook.svg";
-import x from "../assets/icons/twitter.svg";
 
 export const History = ({ paragraphs }) => {
   const [showAll, setShowAll] = useState(false);
@@ -33,51 +29,6 @@ export const History = ({ paragraphs }) => {
           <button className="showParagraphs" onClick={toggleShowAll}>
             {showAll ? "Ocultar" : "Ver más"}
           </button>
-        </div>
-        <div className="history-socialmedia">
-          <h2>¡Saber más!</h2>
-          <div className="social-media">
-            <div className="social-icons instagram">
-              <a
-                target="_blank"
-                href="https://www.instagram.com/nxtwaaave/"
-                rel="noreferrer"
-              >
-                <img src={instragram} alt="instagram" />
-                <p>Instagram</p>
-              </a>
-            </div>
-            <div className="social-icons youtube">
-              <a
-                target="_blank"
-                href="https://www.youtube.com/@NxtWaveBand"
-                rel="noreferrer"
-              >
-                <img src={youtube} alt="youtube" />
-                <p>Youtube</p>
-              </a>
-            </div>
-            <div className="social-icons facebook">
-              <a
-                target="_blank"
-                href="https://www.facebook.com/nxtwaaave"
-                rel="noreferrer"
-              >
-                <img src={facebook} alt="facebook" />
-                <p>Facebook</p>
-              </a>
-            </div>
-            <div className="social-icons x">
-              <a
-                target="_blank"
-                href="https://twitter.com/nxtwaaave"
-                rel="noreferrer"
-              >
-                <img src={x} alt="x" />
-                <p>X</p>
-              </a>
-            </div>
-          </div>
         </div>
       </div>
     </div>
